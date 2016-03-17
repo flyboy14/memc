a1b39f52255d96842532b80fff8da36c07f3c191
flyboy14 <say.bzzzzz@gmail.com>
2016-03-18 01:53:00 +0300

    fixes

--- delete_by_key.py
+++ delete_by_key.py
@@ -3,17 +3,17 @@ import subprocess
 import sys
 import re
 
-server=sys.argv[1]
-port=sys.argv[2]
-prefix=sys.argv[3]
+server = sys.argv[1]
+port = sys.argv[2]
+prefix = sys.argv[3]
 mc = memcache.Client([server+':'+port], debug=0)
 #mc.set("key", "value")
 #mc.set("notkey", "value")
-output = subprocess.Popen(["memcdump", "--server="+server], stdout=subprocess.PIPE).communicate()[0]
-output=output.split("\n")
+output = subprocess.Popen(["memcdump", "--server=" + server], stdout = subprocess.PIPE).communicate()[0]
+output = output.split("\n")
 #print(output)
 for key in output:
-	result = re.match(prefix+'*', key)
+	result = re.match(prefix + '*', key)
 	if result:
 		mc.delete(key)
 		output.remove(key)
